252c1a9870d2deb84af11b5790a09ec4864d09d9
Michel Karam <karammichel920@gmail.com>
2025-09-01 04:21:01 -0700

    Implemented sharp Image composition to ease user comprehension of understanding trade inbound details

--- Arizon-Ad.js
+++ Arizon-Ad.js
@@ -2,8 +2,7 @@ import config from "./config.json" with {type : "json"};
 import {Client, Events, GatewayIntentBits, WebhookClient} from "discord.js";
 import "dotenv/config";
 import fs from "fs";
-
-
+import sharp from "sharp";
 
 
 
@@ -40,9 +39,28 @@ async function run_send_inbound(){
 }
 
 
+
+async function grab_assets(asset_IDS){
+
+    const asset_images = await fetch("https://thumbnails.roblox.com/v1/assets?assetIds=" + asset_IDS + "&size=150x150&isCircular=false&format=Png", {
+        method : "GET",
+        headers : {
+            "Accept" : "application/json"
+        }
+        });
+
+    const response = await asset_images.json();
+
+    return response;
+
+}
+
+
+
 async function send_inbound(){
 
 
+    
 
     const inbounds = await fetch("https://trades.roblox.com/v1/trades/inbound?limit=10", {
         method : "GET",
@@ -71,47 +89,83 @@ async function send_inbound(){
         const hold_details = await inbound_detail.json();
 
 
-        console.log(hold_details);
+        const assetsA = [];
+        const assetsB = [];
+
+        for(let i = 0; i < (hold_details.participantAOffer.items).length; ++i){
+            assetsA.push(Number(hold_details.participantAOffer.items[i].itemTarget.targetId));
+        }
+
+        for(let i = 0; i < (hold_details.participantBOffer.items).length; ++i){
+            assetsB.push(Number(hold_details.participantBOffer.items[i].itemTarget.targetId));
+        }
+
+        const user = await grab_assets(assetsA)
+        const requester = await grab_assets(assetsB);
+
+        console.log(requester);
+
+        const assetsA_imgs = [];
+        const assetB_imgs = [];
+
+        for(let i = 0; i < (user.data).length; ++i){
+            assetsA_imgs.push(user.data[i].imageUrl);
+        }
+
+        for(let i = 0; i < (requester.data).length; ++i){
+            assetB_imgs.push(requester.data[i].imageUrl);
+        }
+
+        console.log(assetB_imgs);
+        
 
 
-        const grab_user_image = await fetch("https://www.roblox.com/users/" + config.roblox_id + "/profile", {
+
+        const hold_userIDS = [hold_details.participantAOffer.user.id, hold_details.participantBOffer.user.id];
+        const image_users = await fetch("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=" + hold_userIDS + "&size=150x150&format=Png&isCircular=true", {
             method : "GET",
             headers : {
-                "Accept" : "text/html"
+                "Accept" : "application/json"
             }
         });
 
-        const grab_html = await grab_user_image.text();
-        console.log(grab_html);
+        const hold_images = await image_users.json();
+
+
+        const result = await buildTradeImg("bin/Complete_Frame.png", ["bin/Blank_Frame.png", "bin/Trade_Frame.png"], assetsA_imgs, assetB_imgs);
+
 
 
         await hook.send({
         username: hold_details.participantAOffer.user.displayName,
-        avatarURL: "https://example.com/avatar.png",
+        avatarURL: hold_images.data[0].imageUrl,
+
+        files : [
+            {
+            attachment : result,
+            name : "trade.png"
+            }
+        ],
         embeds: [
             {
-            title: "NEW TRADE INBOUND BY: " + hold_details.participantBOffer.user.displayName,
+            title: "NEW TRADE INBOUND FROM: " + hold_details.participantBOffer.user.displayName,
             url: "https://www.roblox.com/trades?tab=Inbound",
-            description: "All tests passed.",
-            color: 0x00B894,                    // integer RGB (hex allowed)
-            timestamp: new Date().toISOString(),// shows a small time next to footer
+            color: 0x00B894,
+            timestamp: new Date().toISOString(),
             author: {
-                name: "CI Runner",
-                url: "https://example.com",
-                icon_url: "https://example.com/ci.png"
+                name: "Arizon Trade Inbound Notifier",
             },
-            thumbnail: { url: "https://example.com/thumb.png" },
-            image:     { url: "https://example.com/large.png" },
+            image:     { url: "attachment://trade.png" },
             fields: [
                 { name: "Commit", value: "`abc123`", inline: true },
                 { name: "Duration", value: "2m 14s", inline: true },
                 { name: "Env", value: "prod", inline: true }
             ],
             footer: {
-                text: "Powered by MyBot • v1.2.3",
+                text: "Powered by Arizon • v1.0",
                 icon_url: "https://example.com/footer-icon.png"
             }
-            }
+            },
         ]
         });
 
@@ -119,6 +173,87 @@ async function send_inbound(){
 
 
 }
+
+
+async function buildTradeImg(main_frame, overlay_frames, A_itemID_urls, B_itemID_urls){
+    const resize_Main = await sharp(main_frame).resize({width : 1300, height : 620}).toBuffer();
+
+
+    const composites = [];
+
+    const resizeOverlay0 = await sharp(overlay_frames[0]).resize(170, 170).toBuffer();
+    const resizedOverlay1 = await sharp(overlay_frames[1]).resize(725, 200).toBuffer();
+    
+
+
+    composites.push({input : resizeOverlay0, left : 1100, top : 100 });
+    composites.push({input : resizeOverlay0, left : 1100, top : 425 });
+    composites.push({input : resizedOverlay1, left : 50, top : 100});
+    composites.push({input : resizedOverlay1, left : 50, top : 400});
+
+    
+    for(let i = 0; i < A_itemID_urls.length; ++i){
+        const response = await fetch(A_itemID_urls[i], {
+            method : "GET"
+        });
+
+        const resp = await response.arrayBuffer();
+        const buf = Buffer.from(resp);
+
+        if(i == 0){
+            composites.push({input : buf, left : 76, top : 125});
+        }
+        else if(i == 1){
+            composites.push({input : buf, left : 252, top : 125});
+        }
+        else if(i == 2){
+            composites.push({input : buf, left : 432, top : 125});
+        }
+        else if(i == 3){
+            composites.push({input : buf, left : 607, top : 125});
+
+        }
+    }
+
+
+
+    for(let i = 0; i < B_itemID_urls.length; ++i){
+        
+        const response = await fetch(B_itemID_urls[i], {
+            method : "GET"
+        });
+
+        const resp = await response.arrayBuffer();
+        const buf = Buffer.from(resp);
+
+        if(i == 0){
+            composites.push({input : buf, left : 76, top : 425});
+        }
+        else if(i == 1){
+            composites.push({input : buf, left : 252, top : 425});
+        }
+        else if(i == 2){
+            composites.push({input : buf, left : 432, top : 425});
+        }
+        else if(i == 3){
+            composites.push({input : buf, left : 607, top : 425});
+
+        }
+
+
+    }
+
+    
+
+
+    
+
+    return sharp(resize_Main).composite(composites).png().toBuffer();
+}
+
+
+
+
 
 async function check_roli_exists(){
     const result = await fetch("https://rolimons.com");
@@ -240,11 +375,11 @@ async function main(){
         prev_id = Number(fs.readFileSync("storage/Recent_T_ID.txt", "utf8").trim());
     }
 
-
+/*
     if(!(await check_roli_exists())){
         return;
     }
-
+*/
     //run_send_ad();
 
     run_send_inbound();
